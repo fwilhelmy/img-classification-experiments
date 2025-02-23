068d88db70e0decc51c7dfe82c0aab2eabda3ad3
Félix Wilhelmy <fwilhelmy@hotmail.com>
2025-02-23 15:48:13 -0500

    problem1

--- HW1_2025/assignment1_release/mlp.py
+++ HW1_2025/assignment1_release/mlp.py
@@ -21,32 +21,37 @@ class Linear(nn.Module):
     """
     def __init__(self, in_features: int, out_features: int) -> None:
         super(Linear, self).__init__()
-        raise NotImplementedError
+        self.weight = torch.nn.Parameter(torch.Tensor(out_features, in_features))
+        self.bias = torch.nn.Parameter(torch.Tensor(out_features))
     
     def forward(self, input):
         """
             :param input: [bsz, in_features]
             :return result [bsz, out_features]
         """
-        raise NotImplementedError
-
+        return input @ self.weight.t() + self.bias
 
 class MLP(torch.nn.Module):
+    SUPPORTED_ACTIVATION_FN = {'relu': torch.relu, 'tanh': torch.tanh, 'sigmoid': torch.sigmoid}
+
     # 20 points
     def __init__(self, input_size: int, hidden_sizes: List[int], num_classes: int, activation: str = "relu"):
         super(MLP, self).__init__() 
         self.input_size = input_size
+        assert input_size > 0, "The size of the input should be positive"
         self.hidden_sizes = hidden_sizes
         assert len(hidden_sizes) > 1, "You should at least have one hidden layer"
+        assert all([hidden_size > 0 for hidden_size in hidden_sizes]), "The number of neurons in each layer should be positive"
         self.num_classes = num_classes
+        assert num_classes > 1, "The number of classes should be at least 2"
         self.activation = activation
-        assert activation in ['tanh', 'relu', 'sigmoid'], "Invalid choice of activation"
+        assert activation in self.SUPPORTED_ACTIVATION_FN.keys(), "Invalid choice of activation"
         self.hidden_layers, self.output_layer = self._build_layers(input_size, hidden_sizes, num_classes)
         
         # Initializaton
-        self._initialize_linear_layer(self.output_layer)
         for layer in self.hidden_layers:
             self._initialize_linear_layer(layer)
+        self._initialize_linear_layer(self.output_layer)
     
     def _build_layers(self, input_size: int, 
                         hidden_sizes: List[int], 
@@ -60,16 +65,22 @@ class MLP(torch.nn.Module):
             hidden_layers: nn.ModuleList. Within the list, each item has type nn.Module
             output_layer: nn.Module
         """
-        raise NotImplementedError
+        
+        hidden_layers = nn.ModuleList(
+            [Linear(input_size, hidden_sizes[0])] + 
+            [Linear(hidden_sizes[i], hidden_sizes[i+1]) for i in range(len(hidden_sizes)-1)])
+        output_layer = Linear(hidden_sizes[-1], num_classes)
+        return hidden_layers, output_layer
     
     def activation_fn(self, activation, inputs: torch.Tensor) -> torch.Tensor:
         """ process the inputs through different non-linearity function according to activation name """
-        raise NotImplementedError
+        return self.SUPPORTED_ACTIVATION_FN[activation](inputs)
         
     def _initialize_linear_layer(self, module: nn.Linear) -> None:
         """ For bias set to zeros. For weights set to glorot normal """
-        raise NotImplementedError
-        
+        torch.nn.init.zeros_(module.bias)
+        torch.nn.init.xavier_normal_(module.weight)
+      
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         """ Forward images and compute logits.
         1. The images are first fattened to vectors. 
@@ -79,4 +90,8 @@ class MLP(torch.nn.Module):
         :param images: [batch, channels, width, height]
         :return logits: [batch, num_classes]
         """
-        raise NotImplementedError
+        x = images.view(images.size(0), -1)
+        for layer in self.hidden_layers:
+            x = self.activation_fn(self.activation, layer(x))
+        logits = self.output_layer(x)
+        return logits
