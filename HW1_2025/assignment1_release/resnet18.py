4e79a6213df4c57c9b6d68d052cecdd09ab6cf9a
Félix Wilhelmy <fwilhelmy@hotmail.com>
2025-03-03 11:31:52 -0500

    Problem2

--- HW1_2025/assignment1_release/resnet18.py
+++ HW1_2025/assignment1_release/resnet18.py
@@ -18,41 +18,46 @@ class BasicBlock(nn.Module):
             :param stride: The stride of first conv
         """
         super(BasicBlock, self).__init__()
-        # Uncomment the following lines, replace the ? with correct values.
-        #self.conv1 = nn.Conv2d(
-        #    ?, planes, kernel_size=3, stride=?, padding=1, bias=False)
-        #self.bn1 = nn.BatchNorm2d(?)
-        #self.conv2 = nn.Conv2d(planes, ?, kernel_size=3,
-        #                       stride=1, padding=1, bias=False)
-        #self.bn2 = nn.BatchNorm2d(?)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-        #self.shortcut = nn.Sequential()
-        #if stride != 1 or in_planes != planes:
-        #    self.shortcut = nn.Sequential(
-        #        nn.Conv2d(?, ?,
-        #                  kernel_size=1, stride=stride, bias=False),
-        #        nn.BatchNorm2d(?)
-        #    )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+           self.shortcut = nn.Sequential(
+               nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+               nn.BatchNorm2d(planes)
+           )
 
     def forward(self, x):
         # 1. Go through conv1, bn1, relu
+        output = self.conv1(x)
+        output = self.bn1(output)
+        output = F.relu(output)
+
         # 2. Go through conv2, bn
+        output = self.conv2(output)
+        output = self.bn2(output)
+
         # 3. Combine with shortcut output, and go through relu
-        raise NotImplementedError
+        output += self.shortcut(x)
+        output = F.relu(output)
+
+        return output
 
 
 class ResNet18(nn.Module):
     def __init__(self, num_classes=10):
         super(ResNet18, self).__init__()
-        # Uncomment the following lines and replace the ? with correct values
-        #self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-        #                       stride=1, padding=1, bias=False)
-        #self.bn1 = nn.BatchNorm2d(?)
-        #self.layer1 = self._make_layer(64, 64, stride=1)
-        #self.layer2 = self._make_layer(?, 128, stride=2)
-        #self.layer3 = self._make_layer(?, 256, stride=2)
-        #self.layer4 = self._make_layer(?, 512, stride=2)
-        #self.linear = nn.Linear(?, num_classes)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                              stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(64, 64, stride=1)
+        self.layer2 = self._make_layer(64, 128, stride=2)
+        self.layer3 = self._make_layer(128, 256, stride=2)
+        self.layer4 = self._make_layer(256, 512, stride=2)
+        self.linear = nn.Linear(512, num_classes)
 
     def _make_layer(self, in_planes, planes, stride):
         layers = []
@@ -61,8 +66,17 @@ class ResNet18(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, images):
-        """ input images and output logits """
-        raise NotImplementedError
+        output = self.conv1(images)
+        output = self.bn1(output)
+        output = F.relu(output)
+        output = self.layer1(output)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
+        output = F.avg_pool2d(output, 4)
+        output = output.view(output.size(0), -1)
+        output = self.linear(output)
+        return output
 
     def visualize(self, logdir):
         """ Visualize the kernel in the desired directory """
